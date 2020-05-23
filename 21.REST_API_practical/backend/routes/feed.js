d8da90c0134c13870f0a3d4a26148daee3500731
arijitiiest <1999arijitchoudhury@gmail.com>
2020-05-23 15:54:26 +0530

    rest api practical

---
+++ 21.REST_API_practical/backend/routes/feed.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const { body } = require("express-validator/check");
+
+const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
+
+const router = express.Router();
+
+// /GET /feed/posts
+router.get("/posts", isAuth, feedController.getPosts);
+
+// /POST /feed/post
+router.post(
+  "/post",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  isAuth,
+  feedController.createPost
+);
+
+router.get("/post/:postId", isAuth, feedController.getPost);
+
+router.put(
+  "/post/:postId",
+  [
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
+  ],
+  feedController.updatePost
+);
+
+router.delete("/post/:postId", isAuth, feedController.deletePost);
+
+module.exports = router;
